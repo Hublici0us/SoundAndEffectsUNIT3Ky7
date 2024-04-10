5da9eb3883f9e9801ee858cca15666645eaf199e
Jason Ky <744169@student.dusd.net>
2024-04-10 15:29:09 -0700

    movement and spawning
    - Fixed double jumps
    
    - Created bg and obstacle movement
    
    - Created obstacle spawn

--- SFXUnit3KyP7/Assets/Scripts/PlayerController.cs
+++ SFXUnit3KyP7/Assets/Scripts/PlayerController.cs
@@ -6,20 +6,31 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody playerRb;
 
+    public float jumpForce = 10;
+    public float gravityModifier = 2;
+    public bool onGround = true;
+
     // Start is called before the first frame update
     void Start()
     {
         //<> find any type and component
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && onGround == true)
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            onGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
     }
 }
