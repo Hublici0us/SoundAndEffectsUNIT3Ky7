dde165e5e36b873d3c1c7c3e6068554748dc823c
Jason Ky <744169@student.dusd.net>
2024-04-16 15:29:10 -0700

    3.2 and 3.3
    - Finished the jump process
    - Finished the sidescrolling process
    - Finished the animation for running, jumping, and death.

--- SFXUnit3KyP7/Assets/Scripts/PlayerController.cs
+++ SFXUnit3KyP7/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody playerRb;
+    Animator playerAnim;
 
     public float jumpForce = 10;
     public float gravityModifier = 2;
     public bool onGround = true;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
         //<> find any type and component
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
         
     }
@@ -22,15 +25,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && onGround == true)
+        if (Input.GetKeyDown(KeyCode.Space) && onGround == true && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             onGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        onGround = true;
+        
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over! LLLLL");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
     }
 }
--- SFXUnit3KyP7/Assets/Scripts/SpawnManager.cs
+++ SFXUnit3KyP7/Assets/Scripts/SpawnManager.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    public float startSpawnRate = 2;
+    public float SpawnRate = 1.5f;
+
+    private PlayerController playerControl;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        playerControl = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startSpawnRate, SpawnRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle()
+    {
+
+        if (playerControl.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
